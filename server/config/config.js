1cf3afe7b2987c396f0639a76ea85888b725aa65
naghiaei <enaghiaei>
2023-11-28 10:31:55 +0330

    Get values from .env file

--- server/config/config.js
+++ server/config/config.js
@@ -1,25 +1,26 @@
+require('dotenv').config();
 module.exports = global.config = {
     vals: {
         local_ip: {
-            value: "192.168.43.91"
+            value: process.env.LOCAL_IP
         },
         root: {
             ip: "localhost:8081"
         },
         database: {                       
-            user: "collectief_user",
-            password: "collectief_user", 
-            name: "collectief",
-            port: "3307",
-            host:"mysql_db"
+            user: process.env.DB_USER,
+            password: process.env.DB_PASS, 
+            name: process.env.DB_NAME,
+            port: process.env.DB_PORT,
+            host: process.env.DB_HOST
 
         },        
         database_hub_core: {                      
-            user: "root",
-            password: "marco",  
-            name: "collectief_db",
-            port: "3306",
-            host: "maria_db"
+            user: process.env.DB_HUB_USER,
+            password: process.env.DB_HUB_PASS,
+            name: process.env.DB_HUB_NAME,
+            port: process.env.DB_HUB_PORT,
+            host: process.env.DB_HUB_HOST
         }
         // rest of your translation object
     }
